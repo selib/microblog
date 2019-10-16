35939ba7b6a7c89363e3f6b81a277fe04fb2c206
Eder Sebastian <sebastian.eder@tuv.at>
2019-10-16 10:47:46 +0200

    changess

--- web/main.py
+++ web/main.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask import url_for
 from flask import request
 from flask import render_template
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Index Page</h1> <h2><a href="hello">Continue</a><h2>'
+    return '<h1>Index Page</h1> <h2><a href="hello">Continuesss</a><h2>'
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
@@ -26,5 +28,5 @@ def handle_data():
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
     # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
 # [END gae_flex_python_static_files]
